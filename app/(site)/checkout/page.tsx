2b5716b10a90f0965523c3c0a0e7bc5188fa4fe3
Taran Preet <kaurtaran7952gmail.com>
2026-01-31 18:36:57 +0530

    Fix checkout Suspense + disable prerender

--- app/(site)/checkout/page.tsx
+++ app/(site)/checkout/page.tsx
@@ -1,6 +1,8 @@
 import { Suspense } from "react";
 import CheckoutClient from "./CheckoutClient";
 
+export const dynamic = "force-dynamic";
+
 export default function CheckoutPage() {
   return (
     <Suspense fallback={<div>Loading checkout...</div>}>
